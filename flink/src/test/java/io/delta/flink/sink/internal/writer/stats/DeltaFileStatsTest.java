d7c0b5b3bfa6db44d29187d0079a8f12b05634dd
Gopi Madabhushi <gopik@yellow.ai>
2022-06-23 20:30:20 +0530

    fix tests for stats parsing

--- flink/src/test/java/io/delta/flink/sink/internal/writer/stats/DeltaFileStatsTest.java
+++ flink/src/test/java/io/delta/flink/sink/internal/writer/stats/DeltaFileStatsTest.java
@@ -1,9 +1,14 @@
 package io.delta.flink.sink.internal.writer.stats;
 
 import java.io.File;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.parquet.column.statistics.Statistics;
+import org.apache.parquet.hadoop.metadata.ColumnPath;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -63,6 +68,23 @@ public class DeltaFileStatsTest {
         String json = deltaStats.toJson();
         DeltaFileStats deltaFileStats = new DeltaFileStats(schema, null);
         stats = deltaFileStats.fromJson(json);
-        assertEquals("", stats.getColumnStats().toString());
+
+        Map<ColumnPath, Statistics<?>> columnStats = stats.getColumnStats();
+
+        Encoder encoder = Base64.getEncoder();
+        ColumnPath f1 = ColumnPath.get("f1");
+        ColumnPath f2 = ColumnPath.get("f2");
+        ColumnPath f3 = ColumnPath.get("f3");
+
+
+        assertEquals("MA==",encoder.encodeToString(columnStats.get(f1).getMinBytes()));
+        assertEquals("OQ==", encoder.encodeToString(columnStats.get(f1).getMaxBytes()));
+        assertEquals(0, columnStats.get(f1).getNumNulls());
+        assertEquals("MTc=", encoder.encodeToString(columnStats.get(f2).getMinBytes()));
+        assertEquals("ODc=", encoder.encodeToString(columnStats.get(f2).getMaxBytes()));
+        assertEquals(0, columnStats.get(f2).getNumNulls());
+        assertEquals(7, columnStats.get(f3).genericGetMin());
+        assertEquals(23, columnStats.get(f3).genericGetMax());
+        assertEquals(0, columnStats.get(f3).getNumNulls());
     }
 }
